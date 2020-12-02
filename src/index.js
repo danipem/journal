ffbc0ba2825e456e5063a8fc329a135b5c41ce91
danipem <danielpemiranda@gmail.com>
2020-12-02 18:32:53 +0100

    first

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.scss';
+
+import { JournalApp } from './JournalApp';
+
+
+ReactDOM.render(
+  <div className="hola">
+     <JournalApp />,
+  </div>,
+  document.getElementById('root')
+);
